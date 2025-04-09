dba226e8807f78b25fe85cc3bc52060301a8e815
10x0 <aasseess@pm.me>
2025-04-10 00:08:41 +0100

    feat:setup server with database connections

---
+++ src/server.js
@@ -0,0 +1,19 @@
+const dotenv = require('dotenv');
+const connectDB = require('./config/db.js');
+
+dotenv.config();
+connectDB();
+
+
+process.on('uncaughtException', (error) => {
+  console.log(error.message);
+  console.log('Shutting down due to uncaught exception.');
+  process.exit(1);
+});
+
+if (process.env.NODE_ENV !== 'test') {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
